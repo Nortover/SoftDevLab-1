eefd6850185eb1eb440682edc9992f2374cc4409
Nortover <staspushko7@gmail.com>
2026-05-07 21:35:53 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,18 @@
 import java.util.Random;
 
-public class Main {
+public class Main
+{
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
 
         int[][] A = new int[4][2];
 
         Random rand = new Random();
 
-        // Заповнення матриці випадковими числами (-10 до 10)
-        for (int i = 0; i < 4; i++) 
+        for (int i = 0; i < 4; i++)
         {
-            for (int j = 0; j < 2; j++) 
+            for (int j = 0; j < 2; j++)
             {
                 A[i][j] = rand.nextInt(21) - 10;
             }
@@ -19,10 +20,9 @@ public class Main {
 
         System.out.println("Matrix A:");
 
-        // Вивід матриці
         for (int i = 0; i < 4; i++)
         {
-            for (int j = 0; j < 2; j++) 
+            for (int j = 0; j < 2; j++)
             {
                 System.out.print(A[i][j] + "\t");
             }
@@ -31,18 +31,17 @@ public class Main {
 
         int count = 0;
 
-        // Пошук елементів |x| <= 7
-        for (int i = 0; i < 4; i++) 
+        for (int i = 0; i < 4; i++)
         {
-            for (int j = 0; j < 2; j++) 
+            for (int j = 0; j < 2; j++)
             {
-                if (Math.abs(A[i][j]) <= 7) 
+                if (Math.abs(A[i][j]) <= 7)
                 {
                     count++;
                 }
             }
         }
 
-        System.out.println("Елементи |x| <= 7 = " + count);
+        System.out.println("Count of elements |x| <= 7 = " + count);
     }
 }
